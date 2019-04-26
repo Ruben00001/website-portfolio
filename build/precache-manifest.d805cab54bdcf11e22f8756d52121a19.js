e2812f7948021444c34243516bd03ada4ab974a0
Ruben Seveelaventhan <a.ruben00001@gmail.com>
2019-04-26 16:16:38 +0100

    add build files

---
+++ build/precache-manifest.d805cab54bdcf11e22f8756d52121a19.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "16629301c1fb84d5aa912288574089e9",
+    "url": "/static/media/HankenGrotesk-Bold.16629301.ttf"
+  },
+  {
+    "revision": "ac9cdc469106372643f1",
+    "url": "/static/css/main.08b69388.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0645fe2c29d8e70f00b3",
+    "url": "/static/js/2.ac15b5d1.chunk.js"
+  },
+  {
+    "revision": "3e171961b16bdb7577806972cb88b2f8",
+    "url": "/static/media/HankenGrotesk-Medium.3e171961.ttf"
+  },
+  {
+    "revision": "8173ff25546d69ed58d29336278f801d",
+    "url": "/static/media/HankenGrotesk-Light.8173ff25.ttf"
+  },
+  {
+    "revision": "ac9cdc469106372643f1",
+    "url": "/static/js/main.528f217c.chunk.js"
+  },
+  {
+    "revision": "ca4515b9db2c5087ca6120955b15bd87",
+    "url": "/static/media/XX__Amy3.ca4515b9.png"
+  },
+  {
+    "revision": "e3ff640f881aeb2b524222147cefcd40",
+    "url": "/static/media/GentleEnergy-Complete.e3ff640f.JPG"
+  },
+  {
+    "revision": "8169d649fa76b493e75833fce360c6f4",
+    "url": "/static/media/MountApp-Complete.8169d649.JPG"
+  },
+  {
+    "revision": "7d7f17581cc0d4b5f757cea30165fee8",
+    "url": "/static/media/Calculator-complete.7d7f1758.JPG"
+  },
+  {
+    "revision": "f766982bfd995e4aa6e1a66f0a297094",
+    "url": "/static/media/WeatherAPI-complete.f766982b.JPG"
+  },
+  {
+    "revision": "5efbee2f224b8c99700a91d35277950c",
+    "url": "/index.html"
+  }
+];
